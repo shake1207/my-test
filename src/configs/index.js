0157377edf83c4dd46c4e70ce805e5090cbabd44
shakechang <ShakeChang@gangutech.com>
2022-12-21 19:06:06 +0800

    [config] add APIKEY

---
+++ src/configs/index.js
@@ -0,0 +1,22 @@
+
+const APIKEY = 'db5e868c80035e683ce2af4d26f7ae2c';
+
+
+export const api = {
+  https://api.openweathermap.org/data/2.5/weather?q=japan&appid=db5e868c80035e683ce2af4d26f7ae2c
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
