cb93a0c34b780f7e767709c85e19ddb8bb6d79ce
shakechang <ShakeChang@gangutech.com>
2022-12-22 19:05:54 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef } from 'react';
 import styled from 'styled-components';
-
-import { useFetchApi } from './utils';
-import Loading from './components/Loading';
+import uuid from 'react-uuid';
 import Title from './components/Title';
 import InputBox from './components/InputBox';
 import Button from './components/Button';
+import Detail from './components/Detail';
+import Error from './components/Error';
 import './App.css'
 
 const ContainerStyle = styled.main`
-*{outline: 1px solid}
+
   max-width: 80%;
   min-height: 100vh;
   width: 100%;
@@ -23,6 +23,7 @@ const ContainerStyle = styled.main`
   .wrap {
     width: 100%;
     height: 100%;
+    padding: 10px 0;
   }
   .searchBar {
     display: flex;
@@ -37,66 +38,52 @@ const ContainerStyle = styled.main`
     max-width: 20%;
     width: 100%;
   }
-  ul {
-    margin:0;
-    padding: 0;
-    li {
-      list-style: none;
-      display: flex;
-      align-items: center;
-      justify-content:space-between;
-      .rightBox {
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-        span {
-          margin-left: 10px;
-        }
-      }
-    }
-  }
 `
 
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError,setIsError] = useState(false);
   const [historySearchData, setHistorySearchData] = useState([]);
   const cityRef = useRef(null);
   const countryRef = useRef(null);
-  const {data, isLoading, isError} = useFetchApi();
+  
+  const APIKEY = `db5e868c80035e683ce2af4d26f7ae2c`;
 
-  function handleInputClear() {
-    cityRef.current.value = '';
-    countryRef.current.value= '';
-  }
+  const fetchData = async (city = '', country = '') => {
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APIKEY}`;
 
-  function handleSearch() {
-    const history = {
-      country: countryRef?.current.value,
-      name: cityRef?.current.value,
-      time: new Date().toLocaleTimeString(),
-    }
-    setHistorySearchData(prev => ([...prev, {...history}]))
-  }
-  const fakeData = [
-    {
-      country: 'TW',
-      name: 'Taipei',
-      time: '03:15:02 PM'
-    },
-    {
-      country: 'JP',
-      name: 'Osaka',
-      time: '03:15:02 PM'
-    }
-  ];
+    setIsLoading(true);
+    await fetch(api, { method: 'GET' })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        if(data.cod === 200) {
+          const historyData = {
+            country: data?.sys.country,
+            name: data?.name,
+            time: new Date().toLocaleTimeString(),
+            id: uuid(),
+          }
+          setHistorySearchData(prev => (
+            [...prev, {...historyData}]
+            ));
+        }
+      })
+      .catch((error) => setIsError(error))
+      .finally(() => setTimeout(() => setIsLoading(false), 200));
+  };
 
   const inputs = [
     {
       labelName:'City',
       ref: cityRef,
+      id: uuid(),
     },
     {
       labelName: 'Country',
       ref: countryRef,
+      id: uuid(),
     }
   ];
 
@@ -104,51 +91,93 @@ function App() {
     {
       name: 'Search',
       onClickFc:() => handleSearch(),
+      id: uuid(),
     },
     {
       name: 'Clear',
       onClickFc:() => handleInputClear(),
+      id: uuid(),
     },
   ];
-  console.log(new Date().toLocaleTimeString())
 
-  if(isLoading) return (<Loading />)
-  if(isError) return  <h1>error...</h1>
-  console.log(historySearchData)
+  function getApiData(item) {
+    fetchData(item.name, item.country);
+  }
+
+  function handleInputClear() {
+    cityRef.current.value = '';
+    countryRef.current.value= '';
+  }
+
+  function handleSearch() {
+    fetchData(countryRef?.current?.value, cityRef?.current?.value);
+  }
+
+  function handleDelete(id) {
+    setHistorySearchData(prev => prev.filter(filterItem => filterItem.id !== id))
+  }
+
+
   return (
     <ContainerStyle>
       <Title titleName="Today's Weather" />
       <div className="wrap">
         <div className="searchBar">
-          {inputs.map(({labelName, ref}) => (
-            <InputBox key={labelName} labelName={labelName} ref={ref} />
+          {inputs.map(({labelName, ref, id}) => (
+            <InputBox key={id} labelName={labelName} ref={ref} />
           ))}
           <div className="btnZone">
-            {btns.map(({name, onClickFc}) => (
-              <Button key={name} name={name} onClickFc={onClickFc} />
+            {btns.map(({name, onClickFc, id}) => (
+              <Button key={id} name={name} onClickFc={onClickFc} />
             ))}
           </div>
         </div>
-      <Title titleName="Search History" />
-      {historySearchData.length ? (
-        <ul>
-        {historySearchData.map(({name, country, time}, index) => (
-          <li className="listBox">
-            {index + 1}. {name}, {country}
-            <div className="rightBox">
-              <time datetime={time}>{time}</time>
-              <span>icon search</span>
-              <span>icon delete</span>
-            </div>
-            
-          </li>
-        ))}
-      </ul>
-      ) : (<div>No Record</div>)}
-      
+        <Detail isSuccess={data.cod === 200} isLoading={isLoading}>
+          {data.cod === 200 ? (
+            <>
+              <div className="city">{data?.name}</div>
+              <div className="clouds">{data?.weather[0].main}</div>
+              <ul>
+                <li>
+                  <label htmlFor="">Description</label>
+                  <div>{data?.weather[0].description}</div>
+                </li>
+                <li>
+                  <label htmlFor="">Temperature</label>
+                  <div>{`${data?.main.temp_min}℃ ~ ${data?.main.temp_max}℃`}</div>
+                </li>
+                <li>
+                  <label htmlFor="">Humidity</label>
+                  <div>{data?.main.humidity}</div>
+                </li>
+                <li>
+                  <label htmlFor="">Time</label>
+                  <div>{data?.main.humidity}</div>
+                </li>
+              </ul>
+            </>
+            ) : (data.cod === '400' || data.cod === '404' ) ? (
+              <Error errorMsg={data.message} />
+            ) : null}
+        </Detail> 
+        <Title titleName="Search History" />
+          <Detail className="history">
+            {historySearchData.length ? (
+              <ul>
+                {historySearchData.map((item, index) => (
+                  <li className="listBox" key={item.id}>
+                    {index + 1}. {item.name}, {item.country}
+                    <div className="rightBox">
+                      <time dateTime={item.time}>{item.time}</time>
+                      <span onClick={()=> getApiData(item)}>search</span>
+                      <span onClick={()=> handleDelete(item.id)}>delete</span>
+                    </div>
+                    
+                  </li>
+                ))}
+              </ul> ) : (<div>No Record</div>)}
+          </Detail>
       </div>
-
-      {JSON.stringify(data)}
     </ContainerStyle>
   )
 }
