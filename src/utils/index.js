99402d1b8f0495af3d4a22cef657eccdd82f5d50
Chang Shake <shake1207@gmail.com>
2022-12-22 09:30:52 +0800

    [utils] update

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,8 +5,8 @@ export const useFetchApi = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError,setIsError] = useState(false);
 
-  const fetchUsers = async () => {
-    const api = 'https://api.openweathermap.org/data/2.5/weather?q=japan&appid=db5e868c80035e683ce2af4d26f7ae2c';
+  const fetchData = async () => {
+    const api = 'https://api.openweathermap.org/data/2.5/weather?q=JOHOR&appid=db5e868c80035e683ce2af4d26f7ae2c';
 
     await fetch(api, { method: 'GET' })
     .then((res) => res.json())
@@ -17,7 +17,7 @@ export const useFetchApi = () => {
 
 
   useEffect(() => {
-    fetchUsers();
+    fetchData();
   }, []);
 
   return { data, isLoading, isError}
